23beea91d70773f7c59289735c01538d0157d7cf
Junior-Stranner <JuniorStranner@gmail.com>
2025-09-10 14:46:04 -0300

    corrigindo erro de pasta compactada

---
+++ ghm-authmarket/src/main/java/br/com/judev/ghmauthmarket/GhmAuthmarketApplication.java
@@ -0,0 +1,13 @@
+package br.com.judev.ghmauthmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhmAuthmarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GhmAuthmarketApplication.class, args);
+    }
+
+}
---
+++ ghm-authmarket/src/test/java/br/com/judev/ghmauthmarket/GhmAuthmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.judev.ghmauthmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhmAuthmarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
