11e640fc7bdd4c958143251866237b854e8db298
Junior-Stranner <JuniorStranner@gmail.com>
2025-09-10 16:47:56 -0300

    código teste feiti createProduto como template

---
+++ ghm-authmarket/src/main/java/br/com/judev/ghmauthmarket/dto/Usuario/CreateUsuarioRequest.java
@@ -0,0 +1,9 @@
+package br.com.judev.ghmauthmarket.dto.Usuario;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+
+public record CreateUsuarioRequest (@NotBlank String nome, @Size(min = 6) String senha, @Email String email){
+}
