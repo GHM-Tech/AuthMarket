7ee4a12ddb1dba5af29ec5f9e5238d16740fe137
Maicon Rocha <atendimento.maiconrs@gmail.com>
2025-09-17 16:22:27 -0300

    Pedido emplemantado

--- ghm-authmarket/src/main/java/br/com/judev/ghmauthmarket/controller/PedidoController.java
+++ ghm-authmarket/src/main/java/br/com/judev/ghmauthmarket/controller/PedidoController.java
@@ -1,7 +1,14 @@
 package br.com.judev.ghmauthmarket.controller;
 
+import br.com.judev.ghmauthmarket.dto.Pedido.PedidoRequest;
+import br.com.judev.ghmauthmarket.dto.Pedido.PedidoResponse;
+import br.com.judev.ghmauthmarket.entity.Pedido;
 import br.com.judev.ghmauthmarket.service.PedidoService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/pedidos")
@@ -13,5 +20,17 @@ public class PedidoController {
         this.pedidoService = pedidoService;
     }
 
+    @GetMapping
+    public List<PedidoResponse> listagemPedidos(){
+        return ResponseEntity.ok(pedidoService.listarTodos()).getBody();
+    };
+
+
+    @PostMapping("/register/{usuarioId}")
+    public ResponseEntity<PedidoResponse> criarPedido(
+            @PathVariable("usuarioId") Long usuarioId, @Valid @RequestBody PedidoRequest request){
+        PedidoResponse response = pedidoService.criarPedido(usuarioId, request);
+        return ResponseEntity.status(201).body(response);
+    };
 
 }
